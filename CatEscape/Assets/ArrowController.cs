548a3eb96baaaf91b98ebdae17519378ce2b2039
mai1121 <maimaiukawa@gmail.com>
2019-02-11 13:13:03 +0900

    Set hit action

--- CatEscape/Assets/ArrowController.cs
+++ CatEscape/Assets/ArrowController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class ArrowController : MonoBehaviour
 {
+    GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.player = GameObject.Find("player");
     }
 
     // Update is called once per frame
@@ -15,6 +16,16 @@ public class ArrowController : MonoBehaviour
     {
         transform.Translate(0, -0.1f, 0);
         if(transform.position.y < -5.0f)
+        {
+            Destroy(gameObject);
+        } 
+        Vector2 p1 = transform.position;
+        Vector2 p2 = this.player.transform.position;
+        Vector2 dir = p1 - p2;
+        float d = dir.magnitude;
+        float r1 = 0.5f;
+        float r2 = 1.0f;
+        if(d < r1 + r2)
         {
             Destroy(gameObject);
         }
